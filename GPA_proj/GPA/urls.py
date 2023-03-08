b65cb921ca41657f781cede98c5c08986a0bc230
DeenuYT <96741657+DeenuYT@users.noreply.github.com>
2023-03-08 21:14:03 +0530

    Delete GPA_proj directory

--- GPA_proj/GPA/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.welcome, name="welcomepage"),
-    path('home/', views.home, name='home'),
-    path('home/sem1/', views.sem1, name='sem1'),
-    path('home/sem1/calc_sem1/', views.calc_sem1, name="calc_sem1")
-]
